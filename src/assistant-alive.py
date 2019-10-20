28e2645cbf8dbb16c7202d698b8caef0de5470b0
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-10-21 00:11:03 +0200

    🚧 wip: schedule task

--- src/assistant-alive.py
+++ src/assistant-alive.py
@@ -7,14 +7,18 @@ import time
 import json
 from datetime import datetime
 
-cache_file = "cache.json"   # sets the name of the cache file
+cache_file = "/home/pi/assistant.task/src/cache.json"   # sets the name of the cache file
 # open the cache file and read the json
 with open (cache_file, "r") as read_file:
     cache = json.load(read_file)
+# cache_file.close()
 
-status = "ALIVE"
+values = {'status_alive': 'ALIVE'}
+tmp = {'status': 'ALIVE'}
 while 1==1:
-    if (status is "ALIVE"):
+    if (values['status_alive'] == tmp['status']):
         response = requests.get(cache["URL_ALIVE"])    # make a get http request
-        status = response.json()["status"]
-        time.sleep(30)
+        x = tmp['status']
+        tmp['status'] = str(response.json()['status'])
+        print(x == tmp['status'])
+        time.sleep(300)
