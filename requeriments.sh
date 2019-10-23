622aea96696127c47fc10627fab73bb5fbf6e498
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-10-23 21:21:27 +0200

    ✨ add: operative crontab

--- requeriments.sh
+++ requeriments.sh
@@ -1,5 +1,5 @@
 # configure scheduled task #
-(crontab -l ; echo "@reboot sleep 60; python /home/assistant.task/src/assistant-alive.py &") | crontab -
+(crontab -l ; echo "@reboot sleep 60; cd /home/pi/assistant.task/src/ ; /usr/bin/python /home/pi/assistant.task/src/assistant-alive.py & > logfile.txt") | crontab -
 
 # install python dependencies
 sudo sh ./src/dependencies.sh
--- src/assistant-alive.py
+++ src/assistant-alive.py
@@ -13,12 +13,12 @@ with open (cache_file, "r") as read_file:
     cache = json.load(read_file)
 # cache_file.close()
 
-values = {'status_alive': 'ALIVE'}
-tmp = {'status': 'ALIVE'}
+values = {'action_alive': 'ALIVE'}
+tmp = {'action': 'ALIVE'}
 while 1==1:
-    if (values['status_alive'] == tmp['status']):
-        response = requests.get(cache["URL_ALIVE"])    # make a get http request
-        x = tmp['status']
-        tmp['status'] = str(response.json()['status'])
-        print(x == tmp['status'])
+    if (values['action_alive'] == tmp['action']):
+        response = requests.post(cache["URL_ALIVE"])    # make a post http request
+        x = tmp['action']
+        tmp['action'] = str(response.json()['action'])
+        print(x == tmp['action'])
         time.sleep(300)
