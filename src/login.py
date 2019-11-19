15ca587e2eb658dc649168505ebf9748dedc2a4d
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-11-19 18:42:17 +0100

    🚧 wip: login only if fails sending alive request

--- src/login.py
+++ src/login.py
@@ -3,50 +3,52 @@ import time;
 import requests;
 import json;
 
-# retrieve MAC
-macfile = "macfile.txt"
-comando = "ifconfig | grep ether > " + macfile
-os.system(comando) # execute command
+class Login:
+    # initializer
+    def __init__(self):
+        macfile = "/home/pi/assistant.task/src/macfile.txt"
+        comando = "sudo ifconfig | grep ether > " + macfile
+        os.system(comando) # execute command
+        # wait to do more safe
+        time.sleep(2) 
+        # extract MAC
+        with open(macfile, "r") as file: 
+            data = file.readline().replace('ether', '').replace("\t", '').replace("\n", '').replace(" ", '')[:17]
+        with open(macfile, "w") as file:
+            file.write(data)
+        # Generate credentials
+        tmp = data
+        c = "K" + tmp[9:11] + "U" + tmp[15:17] + "i" + tmp[3:5] + "l" + tmp[12:14] + "p" + tmp[0:2] + "P" + tmp[6:8] + "j" 
+        x = '{"user": "' + data + '", "password": "' + c + '"}'
+        credentials = json.loads(x)
+        print('[login] Credentials: ' + str(credentials))
+        self.credentials = credentials
+        self.tokens = 'empty'
 
-# wait to do more safe
-time.sleep(2) 
-
-# extract MAC
-with open(macfile, "r") as file: 
-    data = file.readline().replace('ether', '').replace("\t", '').replace("\n", '').replace(" ", '')[:17]
-with open(macfile, "w") as file:
-    file.write(data)
-
-# Generate credentials
-tmp = data
-c = "K" + tmp[9:11] + "U" + tmp[15:17] + "i" + tmp[3:5] + "l" + tmp[12:14] + "p" + tmp[0:2] + "P" + tmp[6:8] + "j" 
-print(data + " -> " + c)
-x = '{"user": "' + data + '", "password": "' + c + '"}'
-credentials = json.loads(x)
-print(credentials)
-
-# read URL
-# cache_file = "/home/pi/assistant.task/src/cache.json"
-cache_file = "./cache.json"
-with open (cache_file, "r") as read_file:
-    cache = json.load(read_file)
-url = cache["URL_LOGIN"]
-
-retrieved = False
-# send POST
-while (retrieved == False):
-  try:
-    response = requests.post(url, data = credentials)
-    print(response)
-    if (response.status_code == 200):
-      # save tokens
-      tokens = "/home/pi/assistant.task/src/tokens.json"
-      with open(tokens, "w") as file:
-        file.write(response.content)
-      retrieved = True
-    else:
-      print("Cannot connect... waits 30 second to retry")
-      time.sleep(30)
-  except Exception as err:
-    print("Cannot connect to the host... try again in 30 seconds...")
-    time.sleep(30)
+    def signin(self):
+        cache_file = "./cache.json"
+        with open (cache_file, "r") as read_file:
+            cache = json.load(read_file)
+        url = cache["URL_LOGIN"]
+        print("[login] post: " + str(url))
+        try:
+            if (self.tokens == 'empty'):
+                # si tiene los tokens, devuelve true
+                return True
+            else:
+                # si no los tiene, los genera
+                response = requests.post(url, data = self.credentials)
+                print('[login] post...')
+                if (response.status_code == 200):
+                    # save tokens
+                    tokens = '/home/pi/assistant.task/src/tokens.json'
+                    with open(tokens, "w") as file:
+                        file.write(response.content)
+                    self.tokens = response.content
+                    return True
+                else:
+                    print('[login] Cannot sign in: ' + str(response.content))
+                    return False
+        except Exception as err:
+            print('[login] Cannot connect to the host: ' + str(err))
+            return False
--- src/main.py
+++ src/main.py
@@ -7,9 +7,12 @@ import Alive;
 
 while(1==1):
     login = Login.Login()
-    while (login.signin()):
-	tokens = json.loads(login.tokens)
+    semaphore = login.signin()
+    while (semaphore):
+        tokens = json.loads(login.tokens)
         alive = Alive.Alive(tokens["access_token"])
         r = alive.send()
         print('[main] alive: ' + str(r))
-	time.sleep(300)
+        time.sleep(300)
+        # no se puede mandar peticion, logear de nuevo
+        sempahore = r
