d98735488309b6e0801a8f685e04c34e3cf6c10e
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-11-21 21:35:09 +0100

    ♻️refactor: format code

--- src/Alive.py
+++ src/Alive.py
@@ -1,23 +1,26 @@
-import json;
-import requests;
-import os;
+import json
+import requests
+import os
+
 
 class Alive:
-
     def __init__(self, accesstoken):
         self.accesstoken = accesstoken
         cache_file = "./cache.json"
-        with open (cache_file, "r") as read_file:
+        with open(cache_file, "r") as read_file:
             cache = json.load(read_file)
         self.url = cache["URL_ALIVE"]
 
     def send(self):
         try:
             print('auth: ' + str(self.accesstoken))
-            print("url: " + str(self.url)) 
-            response = requests.get('http://virtual.lab.infor.uva.es:65143/device/alive', 
-                headers={'Content-Type':'application/json',
-                'Authorization': str(self.accesstoken)})
+            print("url: " + str(self.url))
+            response = requests.get(
+                'http://virtual.lab.infor.uva.es:65143/device/alive',
+                headers={
+                    'Content-Type': 'application/json',
+                    'Authorization': str(self.accesstoken)
+                })
             print(response)
             if (response.status_code == 200):
                 # it's alive
@@ -25,19 +28,19 @@ class Alive:
                 return True
             elif (response.status_code == 300):
                 print('Ha sido 300')
-		# parse response
+                # parse response
                 t = response.content
-		print(str(t))
-		print('Procediendo a imprimirlo en un archivo...')
+                print(str(t))
+                print('Procediendo a imprimirlo en un archivo...')
                 # TODO: DELETE THIS, ITS TO TEST
-                comando = "sudo echo '"  + t + "' > /home/pi/assistant.task/src/task_retrieved.json"
-                os.system(comando) # execute command
+                comando = "sudo echo '" + t + "' > /home/pi/assistant.task/src/task_retrieved.json"
+                os.system(comando)  # execute command
                 print('Impreso')
-		# receive a new task
-		r = json.loads(t)
-		print('r = json')
-		print('TO DO: ' + str(r[0]['event']))
-		return True
+                # receive a new task
+                r = json.loads(t)
+                print('r = json')
+                print('TO DO: ' + str(r[0]['event']))
+                return True
             else:
                 print('Cannot send alive request: ' + str(response.content))
                 return False
