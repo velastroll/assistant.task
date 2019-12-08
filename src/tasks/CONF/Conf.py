58ed77ff000a4942c0b8c8fa817561cd415319bb
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-12-08 18:49:47 +0100

    wip:  reboot warn on successful task

--- src/tasks/CONF/Conf.py
+++ src/tasks/CONF/Conf.py
@@ -18,24 +18,30 @@ try:
         })
     if (response.status_code == 200):
         # retrieves config
-        response = requests.get(
+        confData = requests.get(
             cache["URL_BASE"] + "/device/conf",
             headers={
                 'Authorization': str(tkns['access_token'])
             })
         # saves it
-        if (response.status_code == 200):
-            conf = './cache/conf.json'
-            with open(conf, "w") as file:
-                file.write(response.content)
-                self.tokens = response.content
+        if (confData.status_code == 200):
+            confDataJSON = json.load(confData.content)
+
+            # save configuration
+            confFILE = './cache/conf.json'
+            with open(confFILE, "w") as file:
+                file.write(confDataJSON['body'])
 
             # informs to server about successful task
             response = requests.get(
-                cache["URL_BASE"] + "/device/conf/" + json.load(response.content)["timestamp"],
+                cache["URL_BASE"] + "/device/conf/" + confDataJSON["timestamp"],
                 headers={
                     'Authorization': str(tkns['access_token'])
                 })
+            if (response.status_code == 200):
+                os.system("echo '[CONF.py] Update configuration: Device should be rebooted")
+            else:
+                os.system("echo '[CONF.py] Cannot do the action"+ str(response.content) +"' > ./logs/log.conf")
     else:
         os.system("echo '[CONF.py] Cannot do the action"+ str(response.content) +"' > ./logs/log.conf")
 except Exception as err:
